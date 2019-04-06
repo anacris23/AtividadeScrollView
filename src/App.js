6c2e1697460e1e744973129a05a15b58ea786bcb
jeffersonrailsonn <jefferson.railsonn@acad.ifma.edu.br>
2019-04-06 17:30:41 -0300

    Concluido

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet,ScrollView, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, ScrollView, Text, View } from 'react-native';
 import Post from './Post';
 
+const post = [
+  { descricao: "Aprendendo React Native", autor: "Ana Cristina", texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+  { descricao: "Aprendendo React Native", autor: "Ana Cristina", texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
+  { descricao: "Aprendendo React Native", autor: "Ana Cristina", texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+]
 
-export default class App extends Component{
+
+export default class App extends Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Post/>
-        <Post/>
-        <Post/>
-    
+      {post.map(content => (
+          <Post  
+          descricao={content.descricao} 
+          autor={content.autor} 
+          texto={content.texto} />
+      ))}
       </ScrollView>
     );
   }
@@ -19,7 +27,7 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'red'
+    backgroundColor: 'red'
 
   },
   welcome: {
--- src/Post.js
+++ src/Post.js
@@ -1,28 +1,51 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class Post extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: 14, color: "blue" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <View style={styles.conteudoCabecario}>
+        <Text style={styles.descricao} >  
+          {this.props.descricao}
+        </Text>
+        <Text style={styles.autor}>
+          {this.props.autor}
+          
+        </Text>
+        </View>
+        <Text style={styles.texto}>
+          {this.props.texto}
         </Text>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor:"#fff",
-        margin: 20,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    margin: 20,
+  },
+conteudoCabecario:{
+margin:15
+},
 
+  texto: {
+    fontSize: 16,
+    color: 'black',
+    margin: 20,
+    textAlign: 'center',
+    marginBottom: 35,
+  },
+  autor: {
+    fontSize: 20,
+
+  },
+
+  descricao: {
+    fontSize: 25,
+    color: 'black'
+
+  }
 });
